dfaa4917073880d9f7f149fa87c0da3e21188a9a
Evan Hensleigh <ehensleigh@toura.com>
2011-11-07 17:24:16 -0500

    BROKEN COMMIT: Creating scaffold for PageHeader component

---
+++ demos/kitchensink/javascript/components/PageHeader.js
@@ -0,0 +1,13 @@
+dojo.provide('client.components.PageHeader');
+
+mulberry.component('PageHeader', {
+  componentTemplate : dojo.cache('client.components', 'PageHeader/PageHeader.haml'),
+
+  prep : function() {
+
+  },
+
+  init : function() {
+
+  }
+});
