eaf80ad4bdec0009b920ec102b69825578e51f56
Evan <futuraprime@gmail.com>
2011-12-04 18:23:57 -0500

    Changed the other command files as well.

--- mulberry/bin/commands/update_themes.rb
+++ mulberry/bin/commands/update_themes.rb
@@ -2,7 +2,7 @@ module Mulberry
   module Command
     class UpdateThemes
       def initialize(args)
-        to_dir = Mulberry.get_app_dir
+        to_dir = Mulberry::PathHelper.get_app_dir
         Mulberry::App.update_themes to_dir
       end
     end
