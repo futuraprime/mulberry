8771b07d67df6663a3eef1feff814b9f86d2b3f4
Evan <futuraprime@gmail.com>
2011-12-04 02:15:33 -0500

    Made 'get root dir' work.

--- mulberry/path_helper.rb
+++ mulberry/path_helper.rb
@@ -3,9 +3,39 @@
 module Mulberry
   class PathHelper
     
-    def self.get_root_dir()
+    def self.get_root_dir( dir=Dir.pwd )
       # returns the root directory of this mulberry app
       # returns false if this is not a mulberry app
+      
+      # we're going to detect the root directory by the presence of a
+      # mulberry-like directory structure
+      
+      # get the current dir
+      dir = File.expand_path( dir ) # File.expand_path( File.dirname(__FILE__) )
+      
+      # when we're at the root, these will be equal
+      until File.split(dir)[0] == File.split(dir)[1]
+        # we found it
+        break if isroot?(dir)
+        # otherwise, keep iterating
+        dir = File.split(dir)[0]
+      end
+      
+      return dir
+    end
+    
+    ROOTFILES = [
+      'config.yml',
+      'sitemap.yml'
+    ]
+    
+    # checks if a directory is the mulberry root directory
+    # we'll content ourselves with looking for config.yml and sitemap.yml
+    def self.isroot?(dir)
+      ROOTFILES.each do |file|
+        check = File.join(dir, file)
+        return false unless File.exists?(check)
+      end
     end
     
     def self.relative_path(from, to, separator)
