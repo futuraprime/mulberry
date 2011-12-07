d68ab8792d5bc54167f96ad25377a0b332a63b20
Evan Hensleigh <ehensleigh@toura.com>
2011-12-07 12:50:37 -0500

    PathHelper requires that the template_creator tests scaffold a full app now.

--- spec/mulberry/template_creator_spec.rb
+++ spec/mulberry/template_creator_spec.rb
@@ -3,18 +3,22 @@ require 'mulberry/template_creator'
 
 describe Mulberry::TemplateCreator do
   before :each do
+    @source_dir = 'testapp'
     @template_dir = 'templates'
-    @tmpdir = Dir.mktmpdir
+
+    Mulberry::App.scaffold(@source_dir, true)
+    Mulberry.paths.set_app_dir(@source_dir)
+    @app = Mulberry::App.new @source_dir
   end
 
   after :each do
-    FileUtils.rm_rf @tmpdir
+    FileUtils.rm_rf @source_dir
   end
 
   it "should create a template with the requested name" do
     Mulberry::TemplateCreator.new('template', 'foo')
 
-    template_dir = File.join @tmpdir, @template_dir
+    template_dir = File.join @source_dir, @template_dir
 
     File.exists?(template_dir).should be_true
     Dir.entries(template_dir).include?('foo.yml').should be_true
@@ -25,7 +29,7 @@ describe Mulberry::TemplateCreator do
 
     Mulberry::TemplateCreator.new('template', tpl)
 
-    template_file = File.join(@tmpdir, @template_dir, "#{tpl}.yml")
+    template_file = File.join(@source_dir, @template_dir, "#{tpl}.yml")
     template = YAML.load_file(template_file)[tpl]
 
     template['screens'].should_not be_nil
