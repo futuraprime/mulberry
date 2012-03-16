bc758e0c82ccca02dfc6961634fbafadb434e97e
Evan Hensleigh <ehensleigh@toura.com>
2012-03-16 12:15:23 -0400

    Added test for setMedia on video Player

--- spec/app/toura/component_video_player_spec.js
+++ spec/app/toura/component_video_player_spec.js
@@ -15,6 +15,9 @@ describe("video player component", function() {
             }
           ]
         }
+      },
+      feed_media = {
+        url: 'http://av.vimeo.com/01780/039/24113681.web?token=1331829715_b4a96dbd1013cbcb9d316abbce7fbc0e'
       };
 
   beforeEach(function() {
@@ -29,6 +32,13 @@ describe("video player component", function() {
   it("should create a video player", function() {
     var c = new C(config).placeAt(t);
     expect(t.querySelector(getRootSelector(c))).toBeTruthy();
+  });
+  
+  it("should play media it's given", function() {
+    var c = new C(config).placeAt(t);
+    c.setMedia(feed_media);
+    
+    expect(c.media).toEqual(feed_media);
   });
 
   it("should destroy the component when it doesn't have any videos", function() {
