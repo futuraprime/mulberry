d3d81ca4a2ee795ac1a353c8d8099281c21d29c0
Evan Hensleigh <ehensleigh@toura.com>
2012-09-19 11:50:15 -0400

    ChildNodes component refactored to support ExternalContent nodes.

--- app/toura/components/ChildNodes.js
+++ app/toura/components/ChildNodes.js
@@ -4,6 +4,7 @@ dojo.require('mulberry._Component');
 
 dojo.declare('toura.components.ChildNodes', mulberry._Component, {
   templateString : dojo.cache('toura.components', 'ChildNodes/ChildNodes.haml'),
+  itemTemplate: Haml(dojo.cache('toura.components', 'ChildNodes/ChildNodesItem.haml')),
   handleClicks : true,
 
   prepareData : function() {
@@ -14,9 +15,23 @@ dojo.declare('toura.components.ChildNodes', mulberry._Component, {
     if (!this.children.length) {
       this.addClass('empty');
     }
+    this.populate(this.itemTemplate, this.children);
   },
 
   _clickHandler : function(t, e) {
     dojo.addClass(t, 'tapped');
+  },
+
+  _updateNodes : function() {
+    this.children = this.node.children || {};
+    this.populate(this.itemTemplate, this.children);
+
+    if (this.children.length > 0) {
+      this.removeClass('empty');
+    }
+  },
+
+  setupConnections : function() {
+    this.connect(this.node, 'externalChildrenAdded', dojo.hitch(this, '_updateNodes'));
   }
 });
