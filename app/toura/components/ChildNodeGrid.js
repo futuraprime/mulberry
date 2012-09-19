e485461ef9cc79366638c57a08303c752c916ccd
Evan Hensleigh <ehensleigh@toura.com>
2012-09-19 13:45:37 -0400

    BROKEN: still working on grid view images...

--- app/toura/components/ChildNodeGrid.js
+++ app/toura/components/ChildNodeGrid.js
@@ -11,6 +11,18 @@ dojo.declare('toura.components.ChildNodeGrid', toura.components.ChildNodes, {
 
   prepareData : function() {
     this.node.populateChildren();
+    this._setupChildren();
+  },
+
+  adjustMarkup : function() {
+    this.itemTemplate = this.isTablet ? this.itemTabletTemplate : this.itemPhoneTemplate;
+
+    this._setupChildren();
+
+    this.inherited(arguments);
+  },
+
+  _setupChildren : function() {
     // TODO: MAP should enforce this restraint
     this.children = dojo.filter(this.node.children || [], function(child) {
       return child.featuredImage !== undefined;
@@ -21,30 +33,24 @@ dojo.declare('toura.components.ChildNodeGrid', toura.components.ChildNodes, {
           size = num > 11 ? 'medium' : 'large';
 
       this['class'] = 'size-' + size;
+
+      if (this.device.os === 'ios') { return ; }
+
+      if (toura.components.ChildNodeGrid.placedCSS) { return; }
+
+      var tpl = dojo.cache('toura.components.ChildNodeGrid', 'child-node-grid.css.tpl'),
+          aspectRatio = 3/4,
+          width = Math.floor(mulberry.app.UI.viewport.width / 2 - 18),
+          height = Math.floor(width * aspectRatio * 1.40),
+          imageHeight = width * aspectRatio,
+          css = dojo.string.substitute(tpl, {
+            width : width,
+            height : height,
+            imageHeight : imageHeight
+          });
+
+      dojo.place(css, document.querySelector('head'));
+      toura.components.ChildNodeGrid.placedCSS = true;
     }
-
-    if (this.device.os === 'ios') { return ; }
-
-    if (toura.components.ChildNodeGrid.placedCSS) { return; }
-
-    var tpl = dojo.cache('toura.components.ChildNodeGrid', 'child-node-grid.css.tpl'),
-        aspectRatio = 3/4,
-        width = Math.floor(mulberry.app.UI.viewport.width / 2 - 18),
-        height = Math.floor(width * aspectRatio * 1.40),
-        imageHeight = width * aspectRatio,
-        css = dojo.string.substitute(tpl, {
-          width : width,
-          height : height,
-          imageHeight : imageHeight
-        });
-
-    dojo.place(css, document.querySelector('head'));
-    toura.components.ChildNodeGrid.placedCSS = true;
-  },
-
-  adjustMarkup : function() {
-    this.itemTemplate = this.isTablet ? this.itemTabletTemplate : this.itemPhoneTemplate;
-
-    this.inherited(arguments);
   }
 });
