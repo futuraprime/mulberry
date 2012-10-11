aa041fda3631cf2386e61cf6daedc4b6319818b8
Evan Hensleigh <futuraprime@gmail.com>
2012-10-11 13:21:34 -0700

    Merge pull request #587 from futuraprime/product-template-click-fix
    Quick fix to make links in the tablet product template child nodes clickable.
    Conflicts:
    	app/toura/components/ChildNodeText.js

--- app/toura/components/ChildNodeText.js
+++ app/toura/components/ChildNodeText.js
@@ -5,5 +5,5 @@ dojo.require('toura.components.ChildNodes');
 dojo.declare('toura.components.ChildNodeText', toura.components.ChildNodes, {
   templateString : dojo.cache('toura.components', 'ChildNodeText/ChildNodeText.haml'),
   itemTemplate : Haml(dojo.cache('toura.components', 'ChildNodeText/ChildNodeTextItem.haml')),
-  handleClicks : true
+  handleClicks : false
 });
